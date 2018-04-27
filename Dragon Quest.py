89f90961e736340038315a61fc54cdb691a25dc6
greeneguard <windaxe@aol.com>
2018-04-27 07:10:17 -0400

    Update Dragon Quest.py

--- Dragon Quest.py
+++ Dragon Quest.py
@@ -7,87 +7,92 @@ import random
 def roll():
     return random.randrange(1,20)
 
-
 class creature():
-    def __init__(self,name, hp, ac, atk, dmg, ini, opponent):
+    def __init__(self, name, hp, ac, atk, dmg, ini, role):
         self.name = name
+        self.role = role
         self.hp = hp
         self.ac = ac
         self.atk = atk
         self.dmg = dmg
-        self.ini = ini
-        self.opponent = opponent
-        self.initiative = 0
+        self.ini = roll() + ini
 
-    #representation of object
     def __repr__(self):
-        return '({} {} {} {} {})'.format(self.name, self.hp, self.atk, self.dmg, self.ini)
-
-    #set function to roll for initiative
-    def roll_initiative(self):
-        initiative = roll() + self.ini
-        return initiative
-
+        return repr((self.name, self.hp, self.ac, self.ini, self.atk, self.dmg))
+    
     #set function for attack. Use variable for attack roll 
-    def roll_attack(self, defender):
+    def roll_attack(self, ac):
         attack = roll() + self.atk
-        print(self.name, "attacks: ", attack)
-        if attack >= defender.ac:
-            print("Hit")
+        if attack - self.atk == 20:
+            return "Crit"
+        elif attack >= ac:
             return True
         else:
-            print("Miss")
             return False
+        return attack
     
     #set function for rolling damage
-    def roll_damage(self, defender):
+    def roll_damage(self):
        damage = random.randrange(1,self.dmg)
-       print(self.name, "deals", damage, "damage!")
-       defender.hp = defender.hp - damage
+       print(damage, " Damage")
+       return damage
 
-#function to make sure neither of the combatants died with the last attack
-def death_check(defender):
-    if defender.hp <= 0:
-        if defender == dragon:
-            print("The dragon is vanquished!")
-        else:
-            print("Our hero has perished.  Run you fools!")
-        return False
-    else:
-        print(defender.name, "HP: ", defender.hp)
-        return True
+dragon = creature("Dragon", 90, 20, 10, 15, 1, False)
+knight = creature("Knight", 45, 25, 10, 8, 3, True)
+squire = creature("Squire", 25, 20, 8, 8, 6, True)
 
-def attack(attacker, defender):
-    if attacker.roll_attack(defender) == True:
-        attacker.roll_damage(defender)
-        death_check(defender)
+#Create combat sequence.
 
-def initiative(creature):
-    return creature.initiative()
-
-dragon = creature('Dragon', 90, 20, 10, 15, 1, 'opp')
-knight = creature('Knight', 45, 25, 10, 8, 4,'opp')
-squire = creature('Squire', 25, 15, 8, 8, 6,'opp')
-
+#select combatants for simulation
 combatant = [dragon, knight, squire]
 
-dragon.opponent = knight
-knight.opponent = dragon
-squire.opponent = dragon
-
-print(len(combatant))
+#Initiative module
+sorted(combatant, key=lambda creature: creature.ini)
 print(combatant)
 
-#Combat sequence
-turn = 0
+turn = 1
+#Continue combat until the dragon or the knight is down to 0 HP
 while dragon.hp > 0 and knight.hp > 0:
-    turn = turn + 1
 
-    for i in range(len(combatant)):
-        combatant[i].initiative = combatant[i].roll_initiative
-        
-    combatants = sorted(combatant, key=lambda combatant: combatant.initiative)
-    for i in range(len(combatants)):
-        attack(combatants[i], combatants[i].opponent)
-        
+
+    for i in combatant:
+        if combatant[i] == knight:
+            #Knight's attack turn
+            print(knight.name, " attacks")
+            if knight.roll_attack(dragon.ac) == "Crit":
+                print("Critical Hit!")
+                dragon.hp = dragon.hp - knight.dmg
+            elif knight.roll_attack(dragon.ac) == True:
+                print("Hit")
+                dragon.hp = dragon.hp - knight.roll_damage()
+            else:
+                print("Miss")
+
+            #Squire's attack turn
+            print(squire.name, " attacks")        
+            if squire.roll_attack(dragon.ac) == "Crit":
+                print("Critical Hit")
+                dragon.hp = dragon.hp - squire.dmg
+            elif squire.roll_attack(dragon.ac) == True:
+               dragon.hp = dragon.hp - squire.roll_damage()
+               print("Dragon HP:  ", dragon.hp)
+            else:
+                print("Miss")
+
+            if combatant[i] == dragon:
+            #Dragon's attack turn
+                print(dragon.name, " attacks")        
+                if dragon.roll_attack(dragon.ac) == "Crit":
+                    knight.hp = knight.hp - dragon.dmg
+                elif dragon.roll_attack(knight.ac) == True:
+                    knight.hp = knight.hp - dragon.roll_damage()
+                    print("Knight HP:  ", knight.hp)
+                else:
+                    print("Miss")
+    
+    turn = turn + 1
+    if dragon.hp <= 0:
+        print("The dragon is vanquished!")
+    elif knight.hp <=0:
+        print("Our knight has fallen.  Run you fools!")
 print(turn)
