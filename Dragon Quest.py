bb05456e7967f2302cf2f2b2f679933c6830f4cf
greeneguard <windaxe@aol.com>
2018-05-07 11:58:38 -0400

    Update Dragon Quest.py
    Fixed bug that caused infinite loop of combat.
    created teamMonster and teamHero variable to keep track of # of creatures on respective sides.  Once one team is down to zero loop ends.

--- Dragon Quest.py
+++ Dragon Quest.py
@@ -3,91 +3,123 @@ import os
 import math
 import random
 
-#set function for basic roll
-def roll():
-    return random.randrange(1,20)
+global monters
+monsters = []
+global heros
+heros = []
 
+#set function for basic roll expected to pass object modifier as arguement
+def roll(modifier):
+    return random.randrange(1,20) + modifier
+
+#create a creature class that will hold a name, hit points, armor class, attack modifier, damage maximium, and initiative
 
 class creature():
-    def __init__(self,name, hp, ac, atk, dmg, ini, opponent):
+    def __init__(self,name, hp, ac, atk, dmg, ini, ct):
         self.name = name
         self.hp = hp
         self.ac = ac
         self.atk = atk
         self.dmg = dmg
         self.ini = ini
-        self.opponent = opponent
-        self.initiative = 0
+        self.creatureType = ct
+
+    #function to get Name attribute
+    def get_name(self):
+        return self.name
 
     #representation of object
     def __repr__(self):
-        return '({} {} {} {} {})'.format(self.name, self.hp, self.atk, self.dmg, self.ini)
+        return '{} Name:{} HP:{} Attack:{} Damage{}\n'.format(self.creatureType, self.name, self.hp, self.atk, self.dmg)
 
-    #set function to roll for initiative
-    def roll_initiative(self):
-        initiative = roll() + self.ini
-        return initiative
+    #function to call attack sequence.  If an attach meets or beats the opponen't ac roll for damage and check to see if the defender lived.
+    def attack(self, defender):
 
-    #set function for attack. Use variable for attack roll 
-    def roll_attack(self, defender):
-        attack = roll() + self.atk
-        print(self.name, "attacks: ", attack)
-        if attack >= defender.ac:
-            print("Hit")
-            return True
-        else:
-            print("Miss")
-            return False
-    
-    #set function for rolling damage
-    def roll_damage(self, defender):
-       damage = random.randrange(1,self.dmg)
-       print(self.name, "deals", damage, "damage!")
-       defender.hp = defender.hp - damage
+        #set function for attack. If the attack is successful, will call to roll damage.  If unsucessful pass turn.
+        def roll_attack(self, defender):
+            attack = roll(self.atk)
+            print(self.name, "attacks", defender.name)
+            if attack >= defender.ac:
+                return True
+            else:
+                print("Miss")
+                return False
 
-#function to make sure neither of the combatants died with the last attack
-def death_check(defender):
-    if defender.hp <= 0:
-        if defender == dragon:
-            print("The dragon is vanquished!")
-        else:
-            print("Our hero has perished.  Run you fools!")
-        return False
-    else:
-        print(defender.name, "HP: ", defender.hp)
-        return True
+        #set function for rolling damage.Print out damage total and deduct from creature's HP
+        def roll_damage(self, defender):
+            damage = random.randrange(1,self.dmg)
+            print(self.name, "deals", damage, "damage!")
+            defender.hp = defender.hp - damage
+            death_check(defender)
 
-def attack(attacker, defender):
-    if attacker.roll_attack(defender) == True:
-        attacker.roll_damage(defender)
-        death_check(defender)
+        #function to make sure neither of the combatants died with the last attack
+        def death_check(defender):
+            if defender.hp <= 0:
+                if defender.creatureType == "monster":
+                    global monsters
+                    print("The", defender.get_name(), " is vanquished!")
+                    global teamMonsters
+                    teamMonsters = teamMonsters - 1
+                    print(teamMonsters, "monsters remain")
+                else:
+                    global heros
+                    print("Our ", defender.get_name(), ' has perished.')
+                    global teamHeros
+                    teamHeros = teamHeros - 1
+                    print(teamHeros, "heros remain")
+            else:
+                print(defender.name, "HP: ", defender.hp)
 
-def initiative(creature):
-    return creature.initiative()
+        if roll_attack(self, defender) == True:
+            roll_damage(self, defender)
 
-dragon = creature('Dragon', 90, 20, 10, 15, 1, 'opp')
-knight = creature('Knight', 45, 25, 10, 8, 4,'opp')
-squire = creature('Squire', 25, 15, 8, 8, 6,'opp')
 
-combatant = [dragon, knight, squire]
+#function to retrive attribue
+def getName():
+    return self.name
 
-dragon.opponent = knight
-knight.opponent = dragon
-squire.opponent = dragon
+    #function to create initiative score for creature
+    def roll_initiative():
+        self.initiativeScore = roll(self.ini)
 
-print(len(combatant))
-print(combatant)
+#create list of creatures to do combat
+creatures = [
+            creature('Frick', 35, 20, 10, 15, 1, 'monster'),
+            creature('Frack', 35, 20, 10, 15, 1, 'monster'),
+            creature('Knight', 45, 25, 10, 8, 4, 'hero'),
+            creature('Squire', 25, 15, 8, 8, 6, 'hero'),
+            creature('Fighter', 35, 20, 10, 10, 2, 'hero')
+            ]
+
+for i in range(len(creatures)):
+    if creatures[i].creatureType == 'monster':
+        monsters.append(creatures[i])
+    elif creatures[i].creatureType == 'hero':
+        heros.append(creatures[i])
+          
+print(monsters)
+print(heros)
+print()
 
 #Combat sequence
+teamMonsters = len(monsters)
+teamHeros = len(heros)
 turn = 0
-while dragon.hp > 0 and knight.hp > 0:
+while teamMonsters > 0 and teamHeros > 0:
     turn = turn + 1
+    print()
+    print("Turn;", turn)
+    for i in range(len(creatures)):
+        if creatures[i].hp > 0:
+            for o in range(len(creatures)):
+                if creatures[o].creatureType != creatures[i].creatureType and creatures[o].hp > 0:
+                    creatures[i].attack(creatures[o])
+                    break
 
-#    for i in range(len(combatant)):
-#        combatant[i].initiative = combatant[i].roll_initiative
-        
-    combatants = sorted(combatant, key=lambda combatant: combatant.initiative)
-    for i in range(len(combatants)):
-        attack(combatants[i], combatants[i].opponent)
-        
-print(turn)
+if teamMonsters == 0:
+    print("The day has been won.  Hip Hip Hooray!")
+    print(heros)
+elif teamHeros == 0:
+    print("All of our heros have follen.  Run you fool!")
+    print(monsters)
+
